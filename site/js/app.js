21858d405d1d7b3c61282d0878698b0b5ea4d1e0
gcarr <gcarr@turnitin.com>
2016-10-24 10:13:37 +0100

    Chapter 5.  Node modules is duplicated

---
+++ site/js/app.js
@@ -0,0 +1,12 @@
+var app = app || {};
+
+$(function(){
+    // var books = [
+    //     {title: 'Javascripts: The Good Parts', author: 'Douglas Crockford', releaseDate: '1998', keywords: 'Javascript'},
+    //     {title: 'The little Book on Coffeescript', author: 'Alex Maccaw', releaseDate: '1998', keywords: 'CoffeeScript'},
+    //     {title: 'Scala for the Impatient', author: 'Horstmann', releaseDate: '2000', keywords: 'Scala'},
+    //     {title:'SQL for Blaggers', author:'philDB', releaseDate: '2000', keywords: 'SQL'},
+    // ];
+
+    new app.LibraryView();
+});
